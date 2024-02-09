e7e90a2ee38397ca52f497ca236850e1406d82f4
kashishsinghyadav <kashishhsinghhh@gmail.com>
2024-02-09 20:52:27 +0530

    added data to database

--- Gemini/loginuser/admin.py
+++ Gemini/loginuser/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from loginuser.models import Emp,Position
 
+class EmpAdmin(admin.ModelAdmin):
+    list_display=('firstname','mobile','empcode','pos')
+
+admin.site.register(Emp,EmpAdmin)
+
+
+class PosAdmin(admin.ModelAdmin):
+    list_display=('title',)
+admin.site.register(Position,PosAdmin)
 # Register your models here.
--- Gemini/loginuser/models.py
+++ Gemini/loginuser/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Position(models.Model):
     title=models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.title
+
 class Emp(models.Model):
     firstname=models.CharField(max_length=13)
     mobile=models.CharField(max_length=15)

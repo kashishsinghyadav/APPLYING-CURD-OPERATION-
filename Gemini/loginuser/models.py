69980af45499fea626d02e15c7e73bf9e2def1be
kashishsinghyadav <kashishhsinghhh@gmail.com>
2024-02-09 20:32:46 +0530

    create model field

--- Gemini/loginuser/models.py
+++ Gemini/loginuser/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class Position(models.Model):
+    title=models.CharField(max_length=50)
+
+class Emp(models.Model):
+    firstname=models.CharField(max_length=13)
+    mobile=models.CharField(max_length=15)
+    empcode=models.CharField(max_length=3)
+    pos=models.ForeignKey(Position,on_delete=models.CASCADE)
+
+
 # Create your models here.
